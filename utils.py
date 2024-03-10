eac76131f3789cafb741e3829bddb5e7fcf658f4
Clay Graham <claytantor@gmail.com>
2024-03-10 11:52:50 -0700

    remove sql encryption

--- utils.py
+++ utils.py
@@ -6,31 +6,9 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Protocol.KDF import PBKDF2, scrypt
 import os
-
 import hashlib
 import hmac
 import random
-
-# load an encrypted database
-def get_db_url_encrypted():
-    password = os.getenv("DB_PASSWORD", "")
-    if password == "":
-        print("DB_PASSWORD environment variable not set")
-        sys.exit(1)
-
-    # get the path of the file
-    # Get the path of the main Python file
-    main_file_path = os.path.abspath(__file__)  # Absolute path of the main Python file
-
-    # Define the relative path of the file you want
-    relative_file_path = "bcl_encrypted.db"
-
-    # Combine the paths to get the absolute path of the file
-    absolute_file_path = os.path.join(os.path.dirname(main_file_path), relative_file_path)
-
-    db_uri = f'sqlite+pysqlcipher://:{password}@/{absolute_file_path}?cipher=aes-256-cfb&kdf_iter=64000'
-
-    return db_uri
 
 def get_db_url():
 
